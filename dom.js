f5ffe2bd3dab873ea8ce27aa56cab246c1121e7e
bhushikha <shikhamishrabhu21@gmail.com>
2023-03-07 13:45:36 +0530

    DOM File

---
+++ dom.js
@@ -0,0 +1,16 @@
+console.dir(document);
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.title);
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body);
+console.log(document.all);
+console.log(document.all[0]);
+console.log(document.forms[0]);
+console.log(document.links);
+console.log(document.images);
+console.log(document.getElementById('header-title'));
+var headerTitle=document.getElementById('header-title');
+console.log(headerTitle)
+
