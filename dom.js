8581c71e384481aa8d03baa93f1fbfe72a7ad816
bhushikha <126794165+bhushikha@users.noreply.github.com>
2023-03-13 12:48:19 +0530

    Update dom.js

--- dom.js
+++ dom.js
@@ -37,3 +37,42 @@ const oddElements = document.querySelectorAll('li:nth-child(odd)');
 for (let i = 0; i < oddElements.length; i++) {
  oddElements[i].style.backgroundColor = 'green';
 }
+var itemList= document.querySelector('#items');
+//parentelement
+console.log(itemList.parentElement);
+itemList.parentElement.style.backgroundColor = '#f4f4f4';
+console.log(itemList.parentElement.parentElement.parentElement);
+//lastElementChild
+console.log(itemList.lastElementChild);
+itemList.lastElementChild.textContent='Hello 4';
+//lastChild
+console.log(itemList.lastChild);
+//firstElementChild
+console.log(itemList.firstElementChild);
+itemList.firstElementChild.textContent='Hello 1';
+//firstChild
+console.log(itemList.firstChild);
+//nextSibling
+console.log(itemList.nextSibling);
+//nextElementSibling
+console.log(itemList.nextElementSibling);
+//previousSibling
+console.log(itemList.previousSibling);
+//previousElementSibling
+console.log(itemList.previousElementSibling);
+itemList.previousElementSibling.style.color= 'green';
+//createElement
+var newDiv= document.createElement('div');
+newDiv.className='Hello';
+newDiv.id='Hello1';
+//setAttribute
+newDiv.setAttribute('title','Hello div');
+//create text node
+newDiv.createTextNode('Hello World');
+//append child
+newDiv.appendChild(newDivText);
+
+console.log(newDiv);
+
+<h1>Hello world Item Lister <span style="display:none">123</span></h1>;
+<li class="list-group-item">Hello world Item 1</li>;
